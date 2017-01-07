aa9e7e399e05b9f151821a93e7a55795fb5c4e4e
Yela <2067930913@qq.com>
2017-01-07 23:27:27 +0800

    edit route

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@
  * Time: 下午2:14
  */
 
-Route::group(['middleware' => ['web']], function () {
+Route::group(['middleware' => ['web','auth']], function () {
 
     Route::resource('index','BackendController');
 
